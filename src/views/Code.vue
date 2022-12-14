c86a8c96e66bcd68129732d6552482784a29c902
kid <1050109853@qq.com>
2022-12-15 00:20:37 +0800

    feature(design): design env ui

--- src/views/Code.vue
+++ src/views/Code.vue
@@ -1,9 +1,61 @@
 <template>
-    <div>code</div>
+    <el-row :gutter="20" v-for="item in users" :key="item.username">
+        <el-col :span="6" v-for="item in users" :key="item.username">
+            <el-card class="box-card" shadow="hover">
+                <template #header>
+                    <div class="card-header">
+                        <span>{{ item.username }}</span>
+                        <el-button class="button" text>测试</el-button>
+                    </div>
+                </template>
+                <div v-for="o in 4" :key="o" class="text item">{{ item.password + o }}</div>
+            </el-card>
+        </el-col>
+    </el-row>
+    <el-upload class="upload-demo" drag action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15" multiple>
+        <el-icon class="el-icon--upload"><upload-filled /></el-icon>
+        <div class="el-upload__text">Drop file here or <em>click to upload</em></div>
+        <template #tip>
+            <div class="el-upload__tip">jpg/png files with a size less than 500kb</div>
+        </template>
+    </el-upload>
 </template>
 
-<script>
-export default {}
+<script lang="ts">
+import { UploadFilled } from "@element-plus/icons-vue"
+
+export default {
+    data() {
+        return {
+            users: [
+                { username: "kid", password: 1 },
+                { username: "kid", password: 1 },
+                { username: "kid", password: 1 },
+                { username: "kid", password: 1 }
+            ]
+        }
+    }
+}
 </script>
 
-<style></style>
+<style>
+.el-row {
+    margin-bottom: 20px;
+}
+.el-row:last-child {
+    margin-bottom: 0;
+}
+.el-col {
+    border-radius: 4px;
+}
+
+.grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+}
+.card-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+</style>
--- src/views/envirnment.vue
+++ src/views/envirnment.vue
@@ -1,52 +1,71 @@
 <template>
     <el-row>
-        <el-col :span="24"
-            ><el-card class="box-card">
+        <el-col :span="12">
+            <el-card class="box-card" shadow="hover">
                 <template #header>
                     <div class="card-header">
-                        <span>hello action</span>
-                        <el-button class="button" text>Operation button</el-button>
+                        <span>服务器环境参数</span>
                     </div>
                 </template>
                 <div v-for="o in 4" :key="o" class="text item">{{ "List item " + o }}</div>
-            </el-card></el-col
-        >
-    </el-row>
-    <el-row>
-        <el-col :span="12"><div class="grid-content ep-bg-purple" /></el-col>
-        <el-col :span="12"><div class="grid-content ep-bg-purple-light" /></el-col>
-    </el-row>
-    <el-row>
-        <el-col :span="8"><div class="grid-content ep-bg-purple" /></el-col>
-        <el-col :span="8"><div class="grid-content ep-bg-purple-light" /></el-col>
-        <el-col :span="8"><div class="grid-content ep-bg-purple" /></el-col>
-    </el-row>
-    <el-row>
-        <el-col :span="6"></el-col>
-        <el-col :span="6"><div class="grid-content ep-bg-purple-light" /></el-col>
-        <el-col :span="6"><div class="grid-content ep-bg-purple" /></el-col>
-        <el-col :span="6"><div class="grid-content ep-bg-purple-light" /></el-col>
-    </el-row>
-    <el-row>
-        <el-col :span="4"
-            ><div class="grid-content ep-bg-purple" />
-            <el-card class="box-card">
+            </el-card>
+        </el-col>
+        <el-col :span="12">
+            <el-card class="box-card" shadow="hover">
                 <template #header>
                     <div class="card-header">
-                        <span>Card name</span>
-                        <el-button class="button" text>Operation button</el-button>
+                        <span>配置信息</span>
                     </div>
                 </template>
                 <div v-for="o in 4" :key="o" class="text item">{{ "List item " + o }}</div>
-            </el-card></el-col
-        >
-        <el-col :span="4"><div class="grid-content ep-bg-purple-light" /></el-col>
-        <el-col :span="4"><div class="grid-content ep-bg-purple" /></el-col>
-        <el-col :span="4"><div class="grid-content ep-bg-purple-light" /></el-col>
-        <el-col :span="4"><div class="grid-content ep-bg-purple" /></el-col>
-        <el-col :span="4"><div class="grid-content ep-bg-purple-light" /></el-col>
+            </el-card>
+        </el-col>
     </el-row>
+    <el-row class="btn-row">
+        <el-col :span="24">
+            <el-dropdown split-button type="primary" @click="getDockerImages" class="docker-drop" @command="handleCommpand">
+                {{ chooseAction }}
+                <template #dropdown>
+                    <el-dropdown-menu>
+                        <el-dropdown-item :command="showImages">{{ showImages }}</el-dropdown-item>
+                        <el-dropdown-item :command="pushImages">{{ pushImages }}</el-dropdown-item>
+                    </el-dropdown-menu>
+                </template>
+            </el-dropdown>
+            <el-button type="primary" style="margin-left: 15px">修改服务器配置</el-button></el-col
+        >
+    </el-row>
+    <el-divider v-if="loadContainer" />
+    <el-row class="container-row"> </el-row>
+    <el-col :span="6" v-if="loadContainer"><div class="grid-content ep-bg-purple-light" /></el-col>
 </template>
+
+<script lang="ts">
+import { ElMessage } from "element-plus"
+import { UploadFilled } from "@element-plus/icons-vue"
+
+export default {
+    data() {
+        return {
+            loadContainer: false,
+            showImages: "查看docker镜像",
+            pushImages: "上传docker镜像",
+            chooseAction: "查看docker镜像",
+            serverConfigs: []
+        }
+    },
+    methods: {
+        getDockerImages() {
+            if (this.chooseAction === this.showImages) {
+                this.loadContainer = !this.loadContainer
+            }
+        },
+        handleCommpand(command) {
+            this.chooseAction = command
+        }
+    }
+}
+</script>
 
 <style lang="scss">
 .el-row {
@@ -63,7 +82,21 @@
     border-radius: 4px;
     min-height: 36px;
 }
+.card-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.text {
+    font-size: 14px;
+}
+
+.item {
+    margin-bottom: 18px;
+}
+
 .box-card {
-    background-color: rgb(132, 228, 42);
+    width: 480px;
 }
 </style>
